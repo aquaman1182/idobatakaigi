a0d1a2b09bccfc602f71637e583d009f70938e3a
takumi <hal.bandotakumi@gmail.com>
2023-01-28 13:08:03 +0900

    create env local

---
+++ .firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBgiBVQYVGnJbDYMiNYPRQNVbZYBb4Hy1E",
+    authDomain: "idobatakaigi-with-ham-54628.firebaseapp.com",
+    databaseURL: "https://idobatakaigi-with-ham-54628-default-rtdb.firebaseio.com",
+    projectId: "idobatakaigi-with-ham-54628",
+    storageBucket: "idobatakaigi-with-ham-54628.appspot.com",
+    messagingSenderId: "427825226976",
+    appId: "1:427825226976:web:9c77dad5c59c0328739c85"
+  }
